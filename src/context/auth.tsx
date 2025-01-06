132f29d86985c06cbf191f224d46a0c6d243d2bc
KauaZs <zeratop89@gmail.com>
2025-01-06 15:01:25 -0300

    feat:  add auth

---
+++ src/context/auth.tsx
@@ -0,0 +1,39 @@
+import axios from "axios";
+import { createContext, ReactNode, useContext, useEffect, useState } from "react";
+import Cookies from 'js-cookie'
+
+interface UserContext {
+    logged: boolean,
+    user: any,
+    setUser: any
+}
+export const AuthContext = createContext<UserContext>({
+    logged: false,
+    user: undefined,
+    setUser: undefined
+})
+
+export const AuthProvider: React.FC<{ children: ReactNode }> = ({
+	children,
+}) => {
+    const [user, setUser] = useState();
+    useEffect(() => {
+        const fetchUser = async () => {
+            const user = await axios.get('/api/auth/user')
+
+            setUser(user?.data);
+            return user?.data;
+        }  
+        console.log(Cookies.get('user_discord'))
+        if (Cookies.get('user_discord'))  {
+            fetchUser()
+        }
+        
+    }, [])
+
+    return (
+        <AuthContext.Provider value={{ logged: !!user, user, setUser }}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
