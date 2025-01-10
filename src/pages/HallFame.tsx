760a7021009fdd416fa2a5942ab6144fe4fd3ce2
KauaZs <zeratop89@gmail.com>
2025-01-10 16:02:42 -0300

    feat: add hallfame route

---
+++ src/pages/HallFame.tsx
@@ -0,0 +1,54 @@
+import { IUserData } from '@/types/user';
+import HallFameCard from '../components/HallFameCard';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import PageNotFound from './Error/PageNotFound';
+
+const HallFame = () => {
+  const [usersHallFame, setUsersHallFame] = useState<IUserData[] | null>(null);
+
+  useEffect(() => {
+    const findUsersFame = async () => {
+      try {
+        const result = await axios.get('/api/users/listHallFame?maxItens=10', {
+          headers: {
+            Authorization: import.meta.env.API_KEY,
+          },
+        });
+        setUsersHallFame(result.data);
+      } catch (error) {
+        
+        setUsersHallFame(null);
+      }
+    };
+
+    findUsersFame();
+  }, []);
+
+  if (!usersHallFame) {
+    return <PageNotFound />;
+  }
+
+  return (
+    <div className="min-h-screen bg-gray-900 text-white">
+      <h1 className="font-semibold text-4xl shadow text-white lg:text-center">
+        HallFame
+      </h1>
+
+      <div className="flex justify-center items-start mt-6">
+        <div className="grid md:grid-cols-2 gap-6 lg:grid-cols-3">
+          {usersHallFame.map((user, i) => (
+            <HallFameCard
+              avatar={user.profileOptions.avatar}
+              displayName={user.profileOptions.displayName}
+              position={i + 1}
+              views={user.profileOptions.stats.views}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HallFame;
--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -4,6 +4,7 @@ import { Fragment } from "react/jsx-runtime"
 import React from "react"
 import Card from "../pages/Card"
 import Home from '../pages/Home'
+import HallFame from "../pages/HallFame"
 const AppRoutes = () => {
     return (
         <BrowserRouter>
@@ -12,6 +13,7 @@ const AppRoutes = () => {
                  <Route path='/u/:user' element={<Card />} />
                  <Route path='*' element={<Home />} /> 
                  <Route path='/home' element={<Home />} /> 
+                 <Route path='/hallfame' element={<HallFame />} /> 
                 </RouterRoutes>
             </Fragment>
         </BrowserRouter>
