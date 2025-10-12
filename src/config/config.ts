9f0bf96b025b4c3d316ecd4e1eaf4ea6ad35d0fa
Kauã <98705115+KauaZs@users.noreply.github.com>
2025-10-12 13:11:23 -0300

    Update config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,9 +1,9 @@
 const config = {
     profileOptions: {
         displayName: "KauaZs",
-        avatar_url: "https://i.imgur.com/UiYbFjI.png",
-        about: "\"Trust in the Lord with all your heart, and lean not on your own understanding. Proverbs 3:5\"",
-        banner: "https://i.imgur.com/24eSA7k.gif"
+        avatar_url: "https://i.imgur.com/YE1TcJU.png",
+        about: "“Chorar não significa fraqueza, mas que sua alma está tentando se curar.”",
+        banner: "https://i.imgur.com/YH0CVM1.png"
     },
     profileLinks: {
         discord: "https://discord.com/users/573812452165156864",
